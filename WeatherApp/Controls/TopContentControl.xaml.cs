b599808b19abc3d98b0e3bbd5cceab68b1b52224
Ivan <ivansergeevich1209@gmail.com>
2024-12-03 09:28:24 +0200

    Add top control

--- WeatherApp/Controls/TopContentControl.xaml.cs
+++ WeatherApp/Controls/TopContentControl.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,56 @@ namespace WeatherApp.Controls
     /// </summary>
     public partial class TopContentControl : UserControl
     {
+
+        private bool IsLanguagePopupOpen = false;
         public TopContentControl()
         {
             InitializeComponent();
+        }
+
+        // Обробник отримання фокусу на TextBox
+        private void SearchTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            searchTextBox.Text = "";
+        }
+
+        // Обробник втрати фокусу на TextBox
+        private void SearchTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(searchTextBox.Text))
+            {
+                    searchTextBox.Text = "Search for a city by name.."; // Відновлення тексту при втраті фокусу
+                            
+            }
+
+            autoCompletePopup.IsOpen = false;
+        }
+
+        // Обробник натискання кнопки закриття вікна
+        private async void Close_This_Window_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+
+            if (window == null) return;
+
+            DoubleAnimation fadeOutAnimation = new DoubleAnimation
+            {
+                From = 1.0,
+                To = 0.0,
+                Duration = new Duration(TimeSpan.FromSeconds(0.5)) // Налаштування анімації зменшення прозорості
+            };
+
+            window.BeginAnimation(UIElement.OpacityProperty, fadeOutAnimation); // Запуск анімації
+
+            await Task.Delay(500); // Затримка для завершення анімації
+
+            window.Close(); // Закриття вікна
+        }
+
+        private void Change_Lang_Button_Click(object sender, RoutedEventArgs e)
+        {
+            IsLanguagePopupOpen = !IsLanguagePopupOpen;
+            LanguagePopup.IsOpen = IsLanguagePopupOpen;
         }
     }
 }
